89366e33de8fb72e40c96dccc06784f9c39ba540
Chaiwat S <iwatth@gmail.com>
2016-04-09 22:54:24 +0700

    Not working solution for D

--- case2016qd/main.go
+++ case2016qd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/big"
 	"os"
 	"strconv"
+	"strings"
 )
 
-const DEBUG = false
+const DEBUG = true
 
 func main() {
 	fin, _ := os.Open(os.Args[1])
@@ -22,32 +24,55 @@ func main() {
 }
 
 func doCase(caseno int, input string) {
+	s := strings.SplitN(input, " ", 3)
+	K, _ := strconv.Atoi(s[0])
+	C, _ := strconv.Atoi(s[1])
+	S, _ := strconv.Atoi(s[2])
 	if DEBUG {
-		fmt.Printf("DEBUG Case #%d: INPUT %s\n", caseno, input)
+		fmt.Printf("DEBUG Case #%d: INPUT %d %d %d\n", caseno, K, C, S)
 	}
 
-	state := []byte(input)
-	nflip := 0
+	ccomb := make(chan string)
+	go combine(K, C, ccomb)
 
-	for i := len(state) - 1; i >= 0; i-- {
-		if state[i] == '-' {
-			flip(state, i)
-			nflip++
-			if DEBUG {
-				fmt.Printf("DEBUG   Flip %s\n", string(state))
+	for comb := range ccomb {
+		if DEBUG {
+			fmt.Printf("DEBUG   %s\n", comb)
+		}
+	}
+}
+
+func combine(K, C int, ccomb chan string) {
+	state := new(big.Int)
+	max := new(big.Int).Exp(big.NewInt(2), big.NewInt(int64(K)), nil)
+
+	for {
+		comb := strings.Replace(fmt.Sprintf("%"+strconv.Itoa(K)+"s", state.Text(2)), " ", "0", -1)
+		transform(comb, comb, C, ccomb)
+		state.Add(state, big.NewInt(1))
+		if state.Cmp(max) >= 0 {
+			break
+		}
+	}
+
+	close(ccomb)
+}
+
+func transform(orig, state string, C int, ccomb chan string) {
+	if C == 1 {
+		ccomb <- state
+		return
+	}
+
+	buf := make([]byte, len(state)*len(orig))
+	for i := 0; i < len(state); i++ {
+		for b := i * len(orig); b < (i+1)*len(orig); b++ {
+			if state[i] == '0' {
+				buf[b] = '0'
+			} else {
+				buf[b] = orig[b%len(orig)]
 			}
 		}
 	}
-
-	fmt.Printf("Case #%d: %d\n", caseno, nflip)
-}
-
-func flip(state []byte, cur int) {
-	for i := 0; i <= cur; i++ {
-		if state[i] == '+' {
-			state[i] = '-'
-		} else {
-			state[i] = '+'
-		}
-	}
+	transform(orig, string(buf), C-1, ccomb)
 }
